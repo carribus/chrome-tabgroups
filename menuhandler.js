c5637e08a5f20e91d7f8b4e62368b73f1c5b8059
carribus <carribus@gmail.com>
2014-06-18 01:25:18 +0200

    Added grouping of tabs in current window

--- menuhandler.js
+++ menuhandler.js
@@ -3,7 +3,7 @@ var menuItems = {};
 
 // create the menu items
 menuItems.GroupInCurrent = createMenuItem("Group similar tabs in this window", onGroupInCurrentClicked);
-menuItems.GroupInNew = createMenuItem("Group similar tabs in new window", onGroupInNewClicked);
+menuItems.GroupInNew = createMenuItem("Group similar tabs into new window", onGroupInNewClicked);
 
 /**
  * Create a Chrome Context Menu item
@@ -36,7 +36,7 @@ function createNewWindowForTab(tab, callback) {
     })
 }
 
-function enumAllTabs(callback) {
+function enumAllTabs(allWindows, callback) {
     var resultArray = [];
     var queryObj = {
         currentWindow: true
@@ -45,10 +45,14 @@ function enumAllTabs(callback) {
     chrome.tabs.query(queryObj, function(tabArray) {
         resultArray = tabArray;
         queryObj.currentWindow = false;
-        chrome.tabs.query(queryObj, function(tabArray) {
-            resultArray = resultArray.concat(tabArray);
+        if ( allWindows ) {
+            chrome.tabs.query(queryObj, function(tabArray) {
+                resultArray = resultArray.concat(tabArray);
+                callback(resultArray);
+            })
+        } else {
             callback(resultArray);
-        })
+        }
     });
 }
 
@@ -73,7 +77,20 @@ function removeUnrelatedTabs(tabArray, refTab) {
  * @param tab
  */
 function onGroupInCurrentClicked(info, tab) {
-    alert('not implemented yet');
+    var host1, host2;
+    enumAllTabs(false, function(tabs) {
+        // sort tabs by host
+        tabs.sort(function(a, b) {
+            host1 = crackURL(a.url).host;
+            host2 = crackURL(b.url).host;
+            console.log('comparing: %s to %s', host1, host2);
+            return host1.localeCompare(host2);
+        });
+
+        for ( var i = 0, len = tabs.length; i < len; i++ ) {
+            chrome.tabs.move(tabs[i].id, {index:i});
+        }
+    });
 }
 
 /**
@@ -82,10 +99,7 @@ function onGroupInCurrentClicked(info, tab) {
  * @param tab
  */
 function onGroupInNewClicked(info, tab) {
-    console.log(info);
-    console.log(tab);
-
-    enumAllTabs(function(tabs) {
+    enumAllTabs(true, function(tabs) {
         tabs = removeUnrelatedTabs(tabs, tab);
         createNewWindowForTab(tabs[0], function(windowId) {
             chrome.tabs.move(tabs, {"index": -1, "windowId": windowId}, function() {
